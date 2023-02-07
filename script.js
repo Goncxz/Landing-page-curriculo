1a5f7b445d38201b23f4e0e6db8499e72c09515f
Daniel gonçalves <108377282+Goncxz@users.noreply.github.com>
2023-02-07 07:25:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function mudouTamanho() {
+    if(window.innerWidth >= 607) {
+       menu2.style.display = 'none'
+    }
+}
+
+
+
+function clickMenu() {
+    if (menu2.style.display == 'block') {
+        menu2.style.display = 'none'
+    } else {
+        menu2.style.display = 'block'
+    }
+}
